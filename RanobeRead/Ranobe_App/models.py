a20dfeb03ecd6457452a43f30585559e37b7e924
Alexander <Joker124rus@gmail.com>
2021-05-23 15:41:03 +0700

    Created models

--- RanobeRead/Ranobe_App/models.py
+++ RanobeRead/Ranobe_App/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    login = models.CharField(max_length=40)
+    email = models.EmailField()
+    password = models.CharField(max_length=40)
+
+class Genre(models.Model):
+    name = models.CharField(max_length=20)
+
+class Author(models.Model):
+    name = models.CharField(max_length=20)
+
+class Ranobe(models.Model):
+    name = models.CharField(max_length=60)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    genres = models.ManyToManyField(Genre, on_delete=models.CASCADE)
+    date = models.DateField()
+    image = models.ImageField()
+    description = models.TextField()
+    japaneseName = models.CharField(max_length=60)
+    englishName = models.CharField(max_length=60)
+    translator = models.CharField(max_length=20)
+    editor = models.CharField(max_length=20)
+    statusOfTranslate = models.CharField(max_length=20)
+    statusOfTitle = models.CharField(max_length=20)
+
+
+class Ranobe_Reading(models.Model):
+    name = models.CharField(max_length=60)
+    product = models.ForeignKey(Ranobe, on_delete=models.CASCADE)
+    text = models.TextField()
+
+class Applicant(models.Model):
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    text = models.TextField()
